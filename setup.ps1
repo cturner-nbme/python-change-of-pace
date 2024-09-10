72c55271016ebc886d66770a385dea422a04cdff
cturner-nbme <cturner@nbme.org>
2024-09-10 10:59:36 -0400

    Added some comments

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,14 @@
+# Scripted for your convenience
+
+
+# Initializes the virtual environment
 python -m venv .venv
+
+# Activates the virtual environment
 .venv\Scripts\Activate.ps1
+
+# Updates pip to latest version
 python -m pip install --upgrade pip
+
+# Installs the required packages
 pip install -r requirements.txt
